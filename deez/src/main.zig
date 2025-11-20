c3dfe1c926b89dc921de577c8f4ef630958de783
Girgis <girgisemad74@gmail.com>
2025-11-20 10:06:40 +0200

    move some folders

--- deez/src/main.zig
+++ deez/src/main.zig
@@ -1,9 +1,14 @@
 const std = @import("std");
 const expect = std.testing.expect;
+const print = std.debug.print;
 
-pub fn main() !void {
-    var x: u4 = 15;
-    std.debug.print("{d}\n", .{x});
-    x +|= 2;
-    std.debug.print("{d}", .{x});
+pub fn main() void {
+    if (foo(1200, 34) != 1234) {
+        print("Test failed: foo(1200, 34) did not return 1234\n", .{});
+    }
+}
+
+inline fn foo(a: i32, b: i32) i32 {
+    std.debug.print("runtime a = {} b = {}", .{ a, b });
+    return a + b;
 }
